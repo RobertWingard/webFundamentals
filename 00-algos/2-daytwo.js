0bfe52f6edf841745631e9988f6ebb2f151a61ff
RobertWingard <RobertWingard@Icloud.com>
2022-08-16 09:29:40 -0700

    algo work

---
+++ 00-algos/2-daytwo.js
@@ -0,0 +1,59 @@
+// var amazing_show1 = "The office"; // this is a variable that is equal to a string
+// var magicsNumber = 32;
+
+// amazing_show1 = "Curb your enthusiasm" // this is an example of reassigning the variable amazingShow1 to be equal to "curb your enthusiasm"
+
+// var isRobSpeakingFactsAboutBasketball = false; // booleans are another data type: true or false
+
+//a data type is a simple piece of information such as a string, number, boolean
+
+// a data structure is something that can store multiple date types (arrays, objects, trees, linked lists)
+
+//an array is a data structure where you can group multiple pieces of information together
+
+// var greatestArtists = ["Drake", "J Cole", "Kendrick", "Kanye", "Tupac", "Beyonce", "Taylor Swift", "Lupe Fiasco"] //array of string
+// //index (positions)       0         1
+
+// //actual values you see in the array are called: ELEMENTS
+
+// var randomNums = [23, 0, 77, 12, 8];//array of numbers
+
+// var mixedArray = ["Potato", 5, false, true, "hello", 9]; //mixed array - mixed data types
+
+
+
+
+
+// console.log("wazzaaa")
+// console.log(greatestArtists[0])
+
+
+//a loop is to do something repeatedly. Example - print every number from 1-100
+
+// for(var i = 1; i <=100; i++){
+//     console.log(i)
+    //this is the body of the for loop
+
+
+
+// for(var i = 0; i<greatestArtists.length; i++){
+//     console.log(greatestArtists[i])
+//     }
+    
+
+
+
+// var a = 25;
+// a = a - 13;
+// console.log(a/2);
+    
+// a = "hello";
+// console.log(a + " hello");
+
+// for(var i=0; i<10; i++) {
+//     console.log(i);
+//     i = i + 3; 
+// }
+    
+// console.log("outside of the loop " + i);
+
